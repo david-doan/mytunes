3af065eb7f2105077fd5a4003a8bebd8453e48ae
david-doan <a.david.doan@gmail.com>
2016-05-05 16:01:17 -0700

    Completes the Songs tests, fetches songs from PARSE

--- client/collections/Songs.js
+++ client/collections/Songs.js
@@ -1,6 +1,34 @@
 // Songs.js - Defines a backbone collection class for songs.
 var Songs = Backbone.Collection.extend({
 
-  model: SongModel
+  model: SongModel,
+  url: 'https://api.parse.com/1/classes/songs',
+
+  initialize: function(){
+
+    this.fetch({
+      // url: 'https://api.parse.com/1/classes/songs',
+      remove: false,
+      type: 'GET',
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('Songs fetched.');
+      },
+      error: function (data) {
+        console.error('Failed to fetch songs', data);
+      }
+    });
+
+  },
+
+  parse: function(response){
+    return response.results;
+  },
+
+  addSongs: function(songDataArr) {
+    for(var i = 0; i < songDataArr.length; i++){
+      this.add(songDataArr[i]);
+    }
+  }
 
 });
