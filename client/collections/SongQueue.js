ea22beb42ba1888dc5b9c3b1f9dd8f46058909ae
david-doan <a.david.doan@gmail.com>
2016-05-04 17:01:46 -0700

    Completes SongQueue test specs

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -3,7 +3,35 @@ var SongQueue = Backbone.Collection.extend({
 
   model: SongModel,
 
-  initialize: function() {
-  }
+  // listen for changes in any of the songModels
 
+  initialize: function() {
+
+    this.on('add', function(){
+      if(this.length === 1){
+       this.playFirst();
+     } 
+    }, this);
+
+    //listen for when a song ends,
+    // removes first song, play next song queued up
+    this.on('ended', function(){
+      if(this.length > 1 ){
+        this.remove( this.at(0) );
+        this.playFirst();
+      }
+    }, this);
+    
+    // listens for a dequeue
+    // then dequeues next song in queue
+    this.on('dequeue',function(){
+      this.remove( this.at(0) );
+    }, this);
+  },
+
+  playFirst: function() {
+    this.at(0).play();
+  }  
 });
+
+
